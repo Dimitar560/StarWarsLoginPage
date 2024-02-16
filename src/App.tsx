fc95402d7279482555a6f5308f0dee031314e4c5
Dimitar Chavdarov <79813310+Dimitar560@users.noreply.github.com>
2024-02-16 19:27:52 +0200

    React router added
    - React router setup added
    - New page created
    - Submit button navigates to /table (if active)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginForm from "./components/LoginForm/LoginForm";
+import TablePage from "./components/TablePage/TablePage";
 
 function App() {
     return (
         <>
-            <LoginForm />
+            <Routes>
+                <Route path="/" element={<LoginForm />} />
+                <Route path="/table" element={<TablePage />} />
+            </Routes>
         </>
     );
 }

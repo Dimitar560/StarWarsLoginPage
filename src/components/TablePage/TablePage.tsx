ab1da2e3d5807093e95c2e283952edea43409019
Dimitar Chavdarov <79813310+Dimitar560@users.noreply.github.com>
2024-02-16 19:59:26 +0200

    Data fetched
    - Table added
    - Data fetched

--- src/components/TablePage/TablePage.tsx
+++ src/components/TablePage/TablePage.tsx
@@ -1,3 +1,61 @@
+import { useEffect, useState } from "react";
+
+interface IResponse {
+    name: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;
+    birth_year: string;
+    gender: string;
+    homeworld: string;
+    films: string[];
+    species: string[];
+    vehicles: string[];
+    starships: string[];
+    created: string;
+    edited: string;
+    url: string;
+}
+
 export default function TablePage() {
-    return <div>TablePage</div>;
+    const [getData, setGetData] = useState<IResponse[]>([]);
+
+    useEffect(() => {
+        fetch("https://swapi.dev/api/people")
+            .then((res) => res.json())
+            .then((data) => setGetData(data.results));
+    }, []);
+
+    console.log(getData);
+
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <td>name</td>
+                        <td>mass</td>
+                        <td>height</td>
+                        <td>hair color</td>
+                        <td>skin color</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {getData?.map((item, i) => {
+                        return (
+                            <tr key={item.name + i}>
+                                <td>{item.name}</td>
+                                <td>{item.mass}</td>
+                                <td>{item.height}</td>
+                                <td>{item.hair_color}</td>
+                                <td>{item.skin_color}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </>
+    );
 }

2700785bdfaf9f2d301b6c4e692c7a38f9c91f9a
skibidiby <skibidiby@outlook.com>
2024-03-28 11:19:18 +0200

    Added a new component for the About page, and a new Navbar component. Also added a new css file for the Navbar component. Delete history and Logout simulation function Login simulation function

--- src/components/elements/inputs/TextInput.tsx
+++ src/components/elements/inputs/TextInput.tsx
@@ -1,27 +1,41 @@
-import {type ChangeEventHandler } from "react";
+import { type ChangeEventHandler } from "react";
 import styles from "./TextInput.module.css";
 
 interface IProps {
-    inputName: string;
-    defaultText: string;
-    inputType: "text" | "password";
-    labelText: string;
-    changeHandler: ChangeEventHandler<HTMLInputElement>
+	inputName: string;
+	defaultText: string;
+	inputType: "text" | "password";
+	labelText: string;
+	id: string;
+	changeHandler: ChangeEventHandler<HTMLInputElement>;
+	onEnterKey?: () => void;
 }
 
-export default function TextInput({ inputName, defaultText, inputType, labelText, changeHandler }: IProps) {
-    return (
-        <div className={styles.inputWrap}>
-            <label className={styles.inputLabel} htmlFor={inputName}>
-                {labelText}
-            </label>
-            <input
-                className={styles.inp}
-                placeholder={defaultText}
-                name={inputName}
-                type={inputType}
-                onChange={changeHandler}
-            />
-        </div>
-    );
+export default function TextInput({
+	inputName,
+	defaultText,
+	inputType,
+	labelText,
+	changeHandler,
+	id,
+	onEnterKey,
+}: IProps) {
+	return (
+		<div className={styles.inputWrap}>
+			<label className={styles.inputLabel} htmlFor={inputName}>
+				{labelText}
+			</label>
+			<input
+				className={styles.inp}
+				placeholder={defaultText}
+				name={inputName}
+				type={inputType}
+				onChange={changeHandler}
+				id={id}
+				onKeyDown={(e) => {
+					onEnterKey && e.key === "Enter" && onEnterKey();
+				}}
+			/>
+		</div>
+	);
 }

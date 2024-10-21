25a906b438443d549d0ca52bad7cbcecd21194ac
Dimitar560 <Dimitar560@gmail.com>
2024-10-21 15:16:36 +0300

    Revert "Update TablePage.tsx"
    This reverts commit 70c01f21950908d1dd297e7dc909bb50c5cbdae5.

--- src/components/TablePage/TablePage.tsx
+++ src/components/TablePage/TablePage.tsx
@@ -7,71 +7,69 @@ import RegularTable from "./RegularTable/RegularTable";
 import Navbar from "../elements/Navbar/Navbar";
 
 export default function TablePage() {
-    const [getData, setGetData] = useState<IResponse[]>([]);
-    const [mobileTable, setMobileTable] = useState(false);
-    const [awaitingResponse, setAwaitingResponse] = useState(false);
-    const [badRequest, setBadRequest] = useState(false);
+	const [getData, setGetData] = useState<IResponse[]>([]);
+	const [mobileTable, setMobileTable] = useState(false);
+	const [awaitingResponse, setAwaitingResponse] = useState(false);
+	const [badRequest, setBadRequest] = useState(false);
 
-    console.log("Test push");
+	//////////////////////////////////////////////////////
+	// Width check
 
-    //////////////////////////////////////////////////////
-    // Width check
+	const [width, setWidth] = useState(window.innerWidth);
+	const updateDimensions = () => {
+		setWidth(window.innerWidth);
+	};
+	useEffect(() => {
+		window.addEventListener("resize", updateDimensions);
+		return () => window.removeEventListener("resize", updateDimensions);
+	}, []);
 
-    const [width, setWidth] = useState(window.innerWidth);
-    const updateDimensions = () => {
-        setWidth(window.innerWidth);
-    };
-    useEffect(() => {
-        window.addEventListener("resize", updateDimensions);
-        return () => window.removeEventListener("resize", updateDimensions);
-    }, []);
+	//////////////////////////////////////////////////////
+	// Mobile table switch
 
-    //////////////////////////////////////////////////////
-    // Mobile table switch
+	useEffect(() => {
+		if (width < 720) {
+			setMobileTable(true);
+		} else {
+			setMobileTable(false);
+		}
+	}, [width]);
 
-    useEffect(() => {
-        if (width < 720) {
-            setMobileTable(true);
-        } else {
-            setMobileTable(false);
-        }
-    }, [width]);
+	//////////////////////////////////////////////////////
+	// Data fecth
 
-    //////////////////////////////////////////////////////
-    // Data fecth
+	useEffect(() => {
+		setAwaitingResponse(true);
+		fetch("https://swapi.dev/api/people")
+			.then((res) => res.json())
+			.then((data) => setGetData(data.results))
+			.catch((err) => err && setBadRequest(true))
+			.finally(() => setAwaitingResponse(false));
+	}, []);
 
-    useEffect(() => {
-        setAwaitingResponse(true);
-        fetch("https://swapi.dev/api/people")
-            .then((res) => res.json())
-            .then((data) => setGetData(data.results))
-            .catch((err) => err && setBadRequest(true))
-            .finally(() => setAwaitingResponse(false));
-    }, []);
+	return (
+		<>
+			{/* Regular table */}
+			<Navbar />
+			{awaitingResponse && <Loading />}
 
-    return (
-        <>
-            {/* Regular table */}
-            <Navbar />
-            {awaitingResponse && <Loading />}
+			{!mobileTable && !awaitingResponse && (
+				<section className={styles.tableWrap}>
+					{!badRequest && <RegularTable data={getData} />}
 
-            {!mobileTable && !awaitingResponse && (
-                <section className={styles.tableWrap}>
-                    {!badRequest && <RegularTable data={getData} />}
+					{badRequest && <h1>Please try again later</h1>}
+				</section>
+			)}
 
-                    {badRequest && <h1>Please try again later</h1>}
-                </section>
-            )}
+			{/* Mobile table */}
 
-            {/* Mobile table */}
+			{mobileTable && !awaitingResponse && (
+				<section className={styles.mobileTableWrap}>
+					{!badRequest && <MobileTable data={getData} />}
 
-            {mobileTable && !awaitingResponse && (
-                <section className={styles.mobileTableWrap}>
-                    {!badRequest && <MobileTable data={getData} />}
-
-                    {badRequest && <h1>Please try again later</h1>}
-                </section>
-            )}
-        </>
-    );
+					{badRequest && <h1>Please try again later</h1>}
+				</section>
+			)}
+		</>
+	);
 }
